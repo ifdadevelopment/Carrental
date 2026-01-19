279fd12e34d3507ad2c625984efc566a1af600a2
ifdadevelopment <ifdadevelopment.team@gmail.com>
2026-01-19 18:23:17 +0530

    ADDnEW

--- src/app/api/lib/axiosInstance.js
+++ src/app/api/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_DEV_API_URL || "http://localhost:3000";
 
 console.log("API BASE URL:", baseURL);
 
